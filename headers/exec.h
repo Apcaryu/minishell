f89203ec26d50f1c070fb8cbfdd9d35438cbd18a
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-12-19 16:19:54 +0100

    add check_command function

--- headers/exec.h
+++ headers/exec.h
@@ -1,6 +1,8 @@
 #ifndef EXEC_H
 # define EXEC_H
 # include <stdio.h>
+#include "../libft_42/includes_libft/libft.h"
+
 void	read_input(char *input);
 
 #endif
--- srcs/exec/read_input.c
+++ srcs/exec/read_input.c
@@ -1,6 +1,13 @@
 #include "../../headers/exec.h"
 
+void	check_command(char *input)
+{
+	if (!ft_strncmp("echo", input, ft_strlen("echo")))
+		printf("output echo command\n");
+}
+
 void	read_input(char *input)
 {
 	printf("input = %s\n", input);
+	check_command(input);
 }
