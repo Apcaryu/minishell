18f07986b5d5c2b619bcf9340e8fec6bdd0284cd
Apcaryu <antoinepellegrin@outlook.fr>
2022-12-28 17:22:15 +0100

    get type of each token for set each element of parser_lst

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -2,14 +2,25 @@
 
 extern t_data	g_data;
 
+void	set_elem_pars(t_elem_pars *elem_pars, t_token token)
+{
+	elem_pars->type = token.type;
+}
+
 void	parser(void)
 {
 	t_token *lex_lst;
+	t_elem_pars *elem_pars;
 
 	lex_lst = g_data.lexer_lst;
 	while (lex_lst != NULL)
 	{
-		printf("type = %d\n", lex_lst->type);
+//		printf("type = %d\n", lex_lst->type); // TODO remove
+		elem_pars = new_elem_pars(&g_data.garb_lst);
+		set_elem_pars(elem_pars, *lex_lst);
+		printf("elem = %p | type = %d\n", elem_pars, elem_pars->type);
+		elem_pars_add_back(&g_data.parser_lst, elem_pars);
+//		printf("last = %p\n", elem_pars_last(g_data.parser_lst)); // TODO remove
 		lex_lst = lex_lst->next;
 	}
 }
