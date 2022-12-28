bc707fab622527c55cce910d3a1577b2a8667a89
Apcaryu <antoinepellegrin@outlook.fr>
2022-12-28 18:04:15 +0100

    add command in elem_pars.cmd

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -2,9 +2,27 @@
 
 extern t_data	g_data;
 
+char	*set_cmd(t_elem_pars *elem_pars, t_token token)
+{
+	unsigned int	idx;
+	char			*cmd;
+
+	idx = 0;
+	printf("content = %s\n", token.content);
+	while (token.content[idx] != ' ' && token.content[idx] !='\0')
+		idx++;
+	cmd = garbage_alloc(&g_data.garb_lst, sizeof(char) * idx + 1);
+	printf("idx = %u\n", idx);
+	ft_strlcpy(cmd, token.content, idx + 1);
+	printf("cmd = %s\n", cmd);
+	return (cmd);
+}
+
 void	set_elem_pars(t_elem_pars *elem_pars, t_token token)
 {
 	elem_pars->type = token.type;
+	if (token.type == COMMAND)
+		elem_pars->cmd = set_cmd(elem_pars, token);
 }
 
 void	parser(void)
@@ -18,7 +36,7 @@ void	parser(void)
 //		printf("type = %d\n", lex_lst->type); // TODO remove
 		elem_pars = new_elem_pars(&g_data.garb_lst);
 		set_elem_pars(elem_pars, *lex_lst);
-		printf("elem = %p | type = %d\n", elem_pars, elem_pars->type);
+		printf("elem = %p | type = %d | cmd = %s\n", elem_pars, elem_pars->type, elem_pars->cmd);
 		elem_pars_add_back(&g_data.parser_lst, elem_pars);
 //		printf("last = %p\n", elem_pars_last(g_data.parser_lst)); // TODO remove
 		lex_lst = lex_lst->next;
