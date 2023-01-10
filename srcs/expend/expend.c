714a2f21403285b80282c901a4c7bbd52884da54
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2023-01-10 16:19:06 +0100

    is_type_word function added in expend.c

--- srcs/expend/expend.c
+++ srcs/expend/expend.c
@@ -1,6 +1,23 @@
 #include "../headers/expend.h"
 
+extern t_data g_data;
+
+t_bool	is_type_word(t_ntype type)
+{
+	if (type == WORD || type == SINGLE_QUOTE || type == DOUBLE_QUOTE)
+		return (true);
+	return (false);
+}
+
 void	expend(void)
 {
-	return ;
+	t_ntoken *token_lst;
+
+	token_lst = g_data.nlexer_lst;
+	while (token_lst != NULL)
+	{
+		if (is_type_word(token_lst->type))
+			printf("is_word\n");
+		token_lst = token_lst->next;
+	}
 }
