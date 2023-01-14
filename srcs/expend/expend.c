c0334059f09802adbd22bf9b6429f9188efd9924
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2023-01-14 17:11:19 +0100

    varenv replacement fixed

--- srcs/expend/expend.c
+++ srcs/expend/expend.c
@@ -50,7 +50,7 @@ char	*variable_name(char *str, unsigned int idx, unsigned int var_size)
 
 	var_name = garbage_alloc(&g_data.garb_lst, sizeof(char) * var_size + 2);
 	ft_strlcpy(var_name, str + idx, var_size + 2);
-	printf("var_name = %s\n", var_name);
+	printf("var_name = %s\n", var_name); // TODO remove
 	return (var_name);
 }
 
@@ -63,11 +63,35 @@ unsigned int	var_content_size(char *var_name)
 	if (tmp == NULL)
 		return (0);
 	size = ft_strlen(tmp);
-	printf("size = %u\n", size);
+	printf("size = %u\n", size); // TODO remove
 	return (size);
 }
 
-char	*include_var_content(char *str, unsigned int *idx, t_varenv varenv)
+unsigned int	final_strlen(const char *str, const t_varenv varenv)
+{
+	unsigned int	str_len;
+	unsigned int	len_out;
+
+	printf("---------- calcul total size ----------\n"); // TODO remove
+	str_len = ft_strlen(str);
+	printf("str_len = %u\n", str_len); // TODO remove
+	if (varenv.var_name != NULL) {
+		str_len -= varenv.var_size + 1;
+		printf("str_len = %u | var_size = %u | result of %u-%u = %u\n", str_len, varenv.var_size, str_len, varenv.var_size, str_len-varenv.var_size); // TODO remove
+	}
+	len_out = str_len;
+	printf("len_out = %u\n", len_out); // TODO remove
+	if (varenv.var_content != NULL) {
+		len_out += varenv.var_content_size +1;
+		printf("len_out = %u | var_content_size = %u | result of %u+%u = %u\n", len_out, varenv.var_content_size, len_out, varenv.var_content_size, len_out+varenv.var_content_size); // TODO remove
+	}
+	len_out++;
+	printf("len_out = %u\n", len_out); // TODO remove
+	printf("---------- end of calcul ----------\n"); // TODO remove
+	return (len_out);
+}
+
+char	*include_var_content(char *str, unsigned int idx, t_varenv varenv)
 {
 	char			*strout;
 	unsigned int	so_idx;
@@ -75,19 +99,20 @@ char	*include_var_content(char *str, unsigned int *idx, t_varenv varenv)
 	if (varenv.var_name == NULL)
 		return (str);
 	so_idx = 0;
-	strout = garbage_alloc(&g_data.garb_lst, sizeof(char) * (ft_strlen(str) + 1 + varenv.var_content_size + 1));
-	printf("total size = %zu\n", (ft_strlen(str) + 1 + varenv.var_content_size + 1));
-	ft_strlcpy(strout, str, *idx + 1);
-	printf("1.strout = %s\n", strout);
-	so_idx += ft_strlen(strout);
+	strout = garbage_alloc(&g_data.garb_lst, sizeof(char) * final_strlen(str, varenv)/*(ft_strlen(str) + 1 + varenv.var_content_size + 1)*/);
+	printf("total size = %u\n", final_strlen(str, varenv)); // TODO remove
+	ft_strlcpy(strout, str, idx + 1);
+	printf("1.strout = %s\n", strout); // TODO remove
+	so_idx = ft_strlen(strout);
 	if (varenv.var_content != NULL) {
 		ft_strlcat(strout + so_idx, varenv.var_content, (/*ft_strlen(str) + 1 + */varenv.var_content_size + 1));
 	}
-	printf("2.strout = %s\n", strout);
-	*idx += varenv.var_size + 1;
-	printf("str + %u = %s\n", *idx, str + *idx);
-	ft_strlcat(strout + *idx, str + *idx, (ft_strlen(str + *idx) + 2));
-	printf("3.strout = %s\n", strout);
+	so_idx = ft_strlen(strout);
+	printf("2.strout = %s | so_idx = %u\n", strout, so_idx); // TODO remove
+	idx += varenv.var_size + 1;
+	printf("str + %u = %s | size = %zu\n", idx, str + idx, ft_strlen(str + idx)); // TODO remove
+	ft_strlcat(strout + so_idx, str + idx, (ft_strlen(str + idx))+1);
+	printf("3.strout = %s\n", strout); // TODO remove
 	return (strout);
 }
 
@@ -98,8 +123,8 @@ void	remove_quote(char *str)
 
 	idx = 0;
 	size = ft_strlen(str);
-	printf("str = %s\n", str);
-	printf("size = %u\n", size);
+	printf("str = %s\n", str); // TODO remove
+	printf("size = %u\n", size); // TODO remove
 	while(idx < size - 1/*str[idx + 1] != '\0'*/)
 	{
 		str[idx] = str[idx + 1];
@@ -109,7 +134,7 @@ void	remove_quote(char *str)
 		str[idx] = '\0';
 	else
 		str[idx - 1] = '\0';
-	printf("str = %s\n", str);
+	printf("str = %s\n", str); // TODO remove
 }
 
 void	set_var_content(t_ntoken *token)
@@ -124,19 +149,20 @@ void	set_var_content(t_ntoken *token)
 		idx = detect_dollar(token->content, idx);
 		if (idx == UINT_MAX || token->content[idx] == '\0')
 			break ;
-		var.var_size = variable_size(token->content, idx);
-		var.var_name = variable_name(token->content, idx, var.var_size);
-		if (ft_isalnum(var.var_name[1]) || var.var_name[1] == '_')
+		if (isalnum(token->content[idx + 1]) || token->content[idx + 1] == '_')
 		{
+			var.var_size = variable_size(token->content, idx);
+			var.var_name = variable_name(token->content, idx, var.var_size);
+			printf("size of %s = %u\n", var.var_name, var.var_size); // TODO remove
 			var.var_content_size = var_content_size(var.var_name);
 			var.var_content = getenv(var.var_name + 1);
-			printf("content of %s = %s\n", var.var_name, var.var_content);
-			token->content = include_var_content(token->content, &idx, var);
+			printf("content of %s = %s\n", var.var_name, var.var_content); // TODO remove
+			token->content = include_var_content(token->content, idx, var);
+			printf("content[%u] = %c\n", idx, token->content[idx]); // TODO remove
+			idx += var.var_content_size;
 		}
-//		var_content_size(variable_name(token->content, idx, variable_size(token->content, idx)));
-//		printf("var_size = %u\n", variable_size(token->content, idx));
-		printf("content[%u] = %c\n", idx, token->content[idx]);
-		idx++;
+		else
+			idx++;
 	}
 	if (token->type == SINGLE_QUOTE || token->type == DOUBLE_QUOTE)
 		remove_quote(token->content);
