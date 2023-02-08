f08d0cacce24cd231de1f08094a479412e1cd7a0
Melodie SHAHRVARI <mshahrvari@gmail.com>
2023-02-08 19:21:50 +0100

    PATH == NULL [OK]

--- srcs/exec/exec_cmd.c
+++ srcs/exec/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: meshahrv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 14:24:01 by meshahrv          #+#    #+#             */
-/*   Updated: 2023/02/08 14:18:09 by meshahrv         ###   ########.fr       */
+/*   Updated: 2023/02/08 19:19:04 by meshahrv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,52 @@ extern t_data	g_data;
 
 char	**get_env(t_exec *exec)
 {
-	int i;
+	int		i;
 	char	**tab;
-
+	// extern char	*environ;
 	i = 0;
-	while (ft_strnstr(g_data.tab[i], "PATH", 4) == 0)
+	while (g_data.tab[i] && ft_strnstr(g_data.tab[i], "PATH", 4) == 0)
 		i++;
-	tab = ft_split(g_data.tab[i] + 5, ':');
-	if (tab == NULL)
+	if (g_data.tab[i] == NULL)
 		return (NULL);
+	tab = ft_split(g_data.tab[i] + 5, ':');
 	return (tab);
 }
+
+// void	env_is_null(t_elem_pars *start)
+// {
+// 	if (*start->args != NULL)
+// 		access(*start->args, X_OK);
+// 	else
+// 	{
+// 		error_msgs(*start->args, "command not found\n");
+// 		clean_cmds(start->args);
+// 		exit(127);
+// 	}
+// }
 
 void	exec_path(t_elem_pars *start, t_exec *exec)
 {
 	int		i;
-	int		j;
 	char	*tmp;
-	char 	**tab;
+	char	**tab;
 	char	*path;
-	
+	int		access_out;
+
 	i = 0;
-	j = 0;
+	access_out = -1;
 	tab = get_env(exec);
 	if (tab == NULL)
 	{
+		dprintf(2, "ENV EST NULLLLLLL\n");
+		// env_is_null(start);
 		if (*start->args != NULL)
-			access(*start->args, X_OK);
+			access_out = access(*start->args, X_OK);
+		if (access_out != 0)
+		{
+			error_msgs(start->cmd, "command not found\n");
+			exit(127);
+		}
 		else
 		{
 			error_msgs(*start->args, "command not found\n");
@@ -74,18 +93,22 @@ void	exec_path(t_elem_pars *start, t_exec *exec)
 	exit(127);
 }
 
-void exec_cmd(t_exec *exec, t_elem_pars *start, t_elem_pars *elem)
+void	exec_cmd(t_exec *exec, t_elem_pars *start, t_elem_pars *elem)
 {
-	int access_out;
-	
+	int	access_out;
+
 	access_out = -1;
-	while (((elem->next != NULL && start != elem) || (elem->next == NULL && start != NULL)) && start->type != COMMAND)
+	while (((elem->next != NULL && start != elem) \
+		|| (elem->next == NULL && start != NULL)) \
+		&& start->type != COMMAND)
 	{
 		start = start->next;
 		if (start == NULL)
+		{
+			// dprintf(2, "BLABLABLABLA\n");
 			exit (127);
+		}
 	}
-	// dprintf(2, "*exec->cmds = %s\n", *exec->cmds);
 	if (start->cmd != NULL)
 		access_out = access(start->cmd, F_OK);
 	if (access_out == 0)
