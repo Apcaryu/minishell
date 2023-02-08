dc0fe80a1838be169b2605fe1dd2a135be1c1a72
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2023-02-08 16:29:13 +0100

    small fix for quote content

--- srcs/lexer/get_content.c
+++ srcs/lexer/get_content.c
@@ -45,7 +45,7 @@ int	size_of_word(char *input, unsigned int idx)
 	return (size_out);
 }
 
-int	size_of_quote(char *input, unsigned int idx)
+int	size_of_quote(char *input, unsigned int idx, char type_quote)
 {
 	unsigned int	sub_idx;
 	int				size_out;
@@ -56,7 +56,7 @@ int	size_of_quote(char *input, unsigned int idx)
 	{
 		size_out++;
 		sub_idx++;
-		if (input[sub_idx] == '\'' || input[sub_idx] == '\"')
+		if (input[sub_idx] == type_quote)
 		{
 			size_out++;
 			sub_idx++;
@@ -80,7 +80,7 @@ char	*set_content(t_token *token, unsigned int *idx)
 	else if (is_word(g_data.input[*idx]))
 		size = size_of_word(g_data.input, *idx);
 	else if (g_data.input[*idx] == '\"' || g_data.input[*idx] == '\'')
-		size = size_of_quote(g_data.input, *idx);
+		size = size_of_quote(g_data.input, *idx, g_data.input[*idx]);
 	if (size == 0)
 		return (NULL);
 	content = garbage_alloc(&g_data.garb_lst, sizeof(char) * size + 1);
