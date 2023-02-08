d0088ead5027c86dac4f3011dc22d3e5d3ac40bc
Melodie SHAHRVARI <mshahrvari@gmail.com>
2023-02-08 19:52:31 +0100

    Norm: heredoc_process[OK]

--- srcs/exec/heredoc_process.c
+++ srcs/exec/heredoc_process.c
@@ -6,7 +6,7 @@
 /*   By: meshahrv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 16:47:04 by meshahrv          #+#    #+#             */
-/*   Updated: 2023/02/08 16:48:30 by meshahrv         ###   ########.fr       */
+/*   Updated: 2023/02/08 19:50:03 by meshahrv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,36 +20,18 @@ void	read_line_heredoc(int fd, t_elem_pars *elem, t_exec *exec)
 	int		len_limit;
 
 	limiter = elem->args[0];
-	// tmp = elem;
-	// count_cmds = 0;
-	dprintf(2, "limiter = %s\n", limiter);
 	while (1)
 	{
 		write(1, "> ", 2);
 		line = get_next_line(0);
-		// line = readline("> ");
 		len_limit = ft_strlen(limiter);
-		
-		// dprintf(2, "len_limit = [%s]\n", line);
 		if (ft_strnstr(line, limiter, len_limit) && line[len_limit] == '\n')
-		{
-			// free(line);
-			// close(fd);
-			break;
-			// exit(0);
-		}
+			break ;
 		write(fd, line, ft_strlen(line));
-		// write(fd, "\n", 1);
-		// if (!ft_strncmp())
-		// dprintf(2, "args[0] = %s\n", elem->args[0]);
-		// exit(1);
-		// close(fd);
 		free(line);
 	}
 	free(line);
 	close(fd);
-	// if (exec->nbr_cmd == 0)
-	// 	exit (0);
 }
 
 int	ft_heredoc(t_elem_pars *elem, t_exec *exec)
