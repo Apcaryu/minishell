5e8272adec4721e42e5d8529c9a85f8501e44dc7
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2023-02-09 13:58:47 +0100

    expend function normed + special_heredoc and call_lex created

--- srcs/expend/expend.c
+++ srcs/expend/expend.c
@@ -171,11 +171,44 @@ void	set_var_content(t_token *token)
 	}
 }
 
+void	special_heredoc(t_token *token_lst)
+{
+	token_lst = token_lst->next;
+	if (token_lst == NULL)
+		return ;
+	else
+	{
+		if (token_lst->type == C_SPACE)
+			token_lst = token_lst->next;
+		if (token_lst == NULL)
+			return ;
+		else
+			token_lst = token_lst->next;
+		if (token_lst == NULL)
+			return ;
+	}
+}
+
+void	call_lex(t_data *data, t_token *token_lst, t_token *start, t_token *end)
+{
+	if (start == NULL)
+		data->lexer_lst = lex_expend(token_lst->content);
+	else
+	{
+		end = token_lst->next;
+		start->next = lex_expend(token_lst->content);
+		token_lst = start;
+		while (token_lst->next != NULL)
+			token_lst = token_lst->next;
+		token_lst->next = end;
+	}
+}
+
 void	expend(void)
 {
-	t_token *token_lst;
+	t_token	*token_lst;
 	t_token	*start;
-	t_token *end;
+	t_token	*end;
 
 	token_lst = g_data.lexer_lst;
 	if (token_lst->type != VARIABLE)
@@ -185,46 +218,13 @@ void	expend(void)
 	while (token_lst != NULL)
 	{
 		if (token_lst->type == HEREDOC)
-		{
-			token_lst = token_lst->next;
-			if (token_lst == NULL)
-				return ;
-			else
-			{
-				dprintf(2, "coucou\n");
-				if (token_lst->type == C_SPACE)
-					token_lst = token_lst->next;
-				if (token_lst == NULL)
-					return ;
-				else
-				{
-					token_lst = token_lst->next;
-					dprintf(2, "\033[33mblop\033[0m\n");
-				}
-				if (token_lst == NULL)
-					return ;
-			}
-		}
+			special_heredoc(token_lst);
 		if (token_lst->type != VARIABLE)
 			start = token_lst;
 		if (is_type_word(token_lst->type))
 			set_var_content(token_lst);
-		if (token_lst->type == VARIABLE) {
-			if (start == NULL)
-				g_data.lexer_lst = lex_expend(token_lst->content);
-			else {
-				end = token_lst->next;
-				start->next = lex_expend(token_lst->content);
-				token_lst = start;
-				while (token_lst->next != NULL) {
-					token_lst = token_lst->next;
-				}
-				token_lst->next = end;
-			}
-		}
+		if (token_lst->type == VARIABLE)
+			call_lex(&g_data, token_lst, start, end);
 		token_lst = token_lst->next;
 	}
-	printf("--------------------------------------------\n\n");
-	print_lst(g_data.lexer_lst);
-	printf("--------------------------------------------\n\n");
 }
