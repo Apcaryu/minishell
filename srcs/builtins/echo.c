11f652fa535d321010c87ce37bc71f7d86e4c68e
Melodie SHAHRVARI <mshahrvari@gmail.com>
2023-02-10 13:03:26 +0100

    Norm: echo.c [OK]

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: meshahrv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 14:29:40 by meshahrv          #+#    #+#             */
-/*   Updated: 2023/02/09 16:22:41 by meshahrv         ###   ########.fr       */
+/*   Updated: 2023/02/10 13:02:11 by meshahrv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,18 @@
 
 extern t_data	g_data;
 
-void	echo_exec(t_elem_pars *elem)
+t_bool	echo_utils(t_elem_pars *elem, int count_args, t_bool n, int i)
 {
-	t_bool	n;
-	int		i;
 	int		si;
-	int		count_args;
 
-	i = 1;
 	si = i;
-	count_args = 0;
-	n = true;
-	if (!elem->args[1])
-		write(1, "\n", 1);
-	while (elem->args[count_args])
-		count_args++;
 	while (i < count_args)
 	{
 		if (i == 1 && elem->args[i][0] == '-')
 		{
 			si++;
 			while (elem->args[i][si] == 'n')
-			{
 				si++;
-			}
 			if (elem->args[i][si] == ' ' || elem->args[i][si] == '\0')
 			{
 				i++;
@@ -52,6 +40,23 @@ void	echo_exec(t_elem_pars *elem)
 		}
 		i++;
 	}
+	return (n);
+}
+
+void	echo_exec(t_elem_pars *elem)
+{
+	t_bool	n;
+	int		i;
+	int		count_args;
+
+	i = 1;
+	n = true;
+	count_args = 0;
+	if (!elem->args[1])
+		write(1, "\n", 1);
+	while (elem->args[count_args])
+		count_args++;
+	n = echo_utils(elem, count_args, n, i);
 	if (n)
 		write(1, "\n", 1);
 }
