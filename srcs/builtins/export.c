1dfdc929cb3919f0671c9ac8c771ca1b50ecced1
Melodie SHAHRVARI <mshahrvari@gmail.com>
2023-02-10 12:30:53 +0100

    Norm: export.c [1/2] | Todo: 25 lines [2/2]

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meshahrv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 12:30:03 by meshahrv          #+#    #+#             */
+/*   Updated: 2023/02/10 12:30:04 by meshahrv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../headers/builtins.h"
 #include "../../headers/minishell.h"
 
@@ -6,16 +18,14 @@ extern t_data	g_data;
 char	*get_syntax(char *str)
 {
 	int		i;
+	int		size;
 	char	*dest;
-	int size;
 
 	if (str == NULL)
 		return (str);
 	size = ft_strlen(str);
 	dest = NULL;
 	i = 0;
-	// dprintf(2, "ICI\n");
-	// dprintf(2, "str = %s\n", str);
 	dest = ft_calloc(sizeof(char), ft_strlen(str) +1);
 	ft_strlcpy(dest, str, size +1);
 	return (dest);
@@ -23,8 +33,8 @@ char	*get_syntax(char *str)
 
 int	env_line_already_exist(t_env *new)
 {
-	t_env	*head;
 	int		len;
+	t_env	*head;
 
 	len = 0;
 	head = g_data.env_bis;
@@ -49,48 +59,34 @@ int	env_line_already_exist(t_env *new)
 
 void	export_exec(char *str)
 {
-
-	// dprintf(2, "exec export\n");
-	t_env	*new;
+	int		i;
 	char	*new_env;
+	t_env	*new;
 	t_data	data;
 
 	data = g_data;
-	// dprintf(2, "ee str = %s\n", str);
-	// dprintf(2, "export = %s\n", data.parser_lst->args[1]);
 	new_env = get_syntax(str);
-	// dprintf(2, "new_env = %s\n", new_env);
-	// dprintf(2, "HALLOOOOwen\n");
 	if (!new_env)
 		return ;
-	// dprintf(2, "new_env = %s\n", new_env);
 	new = new_env_line(new_env);
-	// dprintf(2, "new = %s\n", new->line);
 	if (!new)
 	{
 		free(new_env);
 		return ;
 	}
 	free(new_env);
-	// dprintf(2, "env_bis = %s\n", data.env_bis->line);
 	if (!data.env_bis)
 		data.env_bis = new;
 	else
 	{
-		// dprintf(2, "env_bis = %s\n", data.env_bis->line);
 		if (env_line_already_exist(new) == 0)
 			add_env_line(data.env_bis, new);
-		// free_tab_str(&data->test, -1);
 	}
 	data.tab = convert_lst_to_tab(data);
-	// dprintf(2, "tab = %s\n", *data.tab);
 	if (!data.tab)
 		return ;
-	int i = 0;
-	while (data.tab[i+1] != NULL)
+	i = 0;
+	while (data.tab[i + 1] != NULL)
 		i++;
-	// dprintf(2, "tab[ledernier] = %s | new.line = %s\n", data.tab[i], new->line);
-	// g_data.env_bis = new;
 	g_data.tab = data.tab;
-	// dprintf(2, "tab[ledernier] = %s\n", g_data.tab[i]);
 }
