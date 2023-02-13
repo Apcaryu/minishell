bf7549f0f39d080740abb99d99588964241af6db
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2023-02-13 15:41:09 +0100

    files for error token msg added

---
+++ srcs/error/error_token.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_token.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apellegr <apellegr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 15:24:31 by apellegr          #+#    #+#             */
+/*   Updated: 2023/02/13 15:24:33 by apellegr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/error_msg.h"
+
+t_bool	error_token(t_data *data)
+{
+	t_bool	error_detected;
+
+	error_detected = false;
+	return (error_detected);
+}
