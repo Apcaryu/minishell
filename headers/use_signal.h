11b963b3b7304a94f226f4d960367b6294257cd8
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2023-02-16 16:25:16 +0100

    use_signal.h normed

--- headers/use_signal.h
+++ headers/use_signal.h
@@ -1,8 +1,19 @@
-#ifndef SIGNAL_H
-# define SIGNAL_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   use_signal.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apellegr <apellegr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 16:24:25 by apellegr          #+#    #+#             */
+/*   Updated: 2023/02/16 16:24:25 by apellegr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef USE_SIGNAL_H
+# define USE_SIGNAL_H
 # include "minishell.h"
 
 void	ctrl_c(int code);
-void	ctrl_d(int code);
 
 #endif
